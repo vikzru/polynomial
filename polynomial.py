410a5716f15a3f78722acbde4a520e1eb0d32258
Viktoria Zruttova <zruttova@bu.edu>
2024-01-28 10:49:35 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,7 +5,8 @@ class X:
     def __repr__(self):
         return "X"
 
-
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -14,6 +15,8 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -23,6 +26,8 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -32,7 +37,8 @@ class Sub:
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
 
- 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -40,20 +46,10 @@ class Mul:
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, (Add, Sub)):
-            p1_repr = "(" + repr(self.p1) + ")"
-        else:
-            p1_repr = repr(self.p1)
-        
-        if isinstance(self.p2, (Add, Sub, Mul, Div)):
-            p2_repr = "(" + repr(self.p2) + ")"
-        else:
-            p2_repr = repr(self.p2)
-        
-        return f"{p1_repr} * {p2_repr}"
+        return repr(self.p1) + " * " + repr(self.p2)
 
-
-
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 
 class Div:
     def __init__(self, p1, p2):
@@ -63,23 +59,28 @@ class Div:
     def __repr__(self):
         return "(" + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
 
-
+    def evaluate(self, x):
+        try:
+            return self.p1.evaluate(x) / self.p2.evaluate(x)
+        except ZeroDivisionError:
+            print("Error: Division by zero.")
+            return float('nan')
 
 
 poly_div = Div(Add(Int(10), Mul(X(), Int(2))), Int(2))
 poly_sub = Sub(Add(Int(5), X()), Mul(Int(2), X()))
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 
-print(poly)
-print(poly_div)
-print(poly_sub)
+print(poly, " at x=2 is ", poly.evaluate(2) )
+print(poly_div, " at x=2 is ", poly_div.evaluate(2))
+print(poly_sub, " at x=2 is ", poly_sub.evaluate(2))
 
 # Test case 1: Division and Subtraction in a larger expression
 larger_expr = Add(
     Div(Mul(Int(5), X()), Int(2)),
     Sub(Add(Int(3), X()), Mul(X(), Int(4)))
 )
-print("Result for larger_expr",larger_expr)
+print("Result for larger_expr",larger_expr, " at x = 3 is: ", larger_expr.evaluate(3))
 
 # Test case 2: Complex expression involving all operations
 complex_expr = Add(
@@ -89,7 +90,7 @@ complex_expr = Add(
         Mul(Int(3), X())
     )
 )
-print("Result for complex_expr",complex_expr)
+print("Result for complex_expr",complex_expr, " at x = 3 is: ", complex_expr.evaluate(3))
 
 # Test case 3: Expression with nested operations
 nested_expr = Add(
@@ -99,4 +100,4 @@ nested_expr = Add(
         Mul(X(), Div(Int(4), X()))
     )
 )
-print("Result for nested_expr", nested_expr)
+print("Result for nested_expr", nested_expr, " at x = 3 is: ",nested_expr.evaluate(3))
